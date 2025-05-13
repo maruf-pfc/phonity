48c1a69fff0ffe6fad357b016f08443b834f5464
= <sohelf131@gmail.com>
2025-05-13 13:25:24 +0600

    add: post model and comment model are created for community part

---
+++ server/models/Comments.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const commentsSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    commentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.models['Comment'] || mongoose.model('Comment', commentsSchema)
